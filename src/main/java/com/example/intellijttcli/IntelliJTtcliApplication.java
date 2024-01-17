4873f534b8b6f57fed658e12162ddee14000237a
Christian Huff <christian.huff.dev@gmail.com>
2024-01-17 06:39:44 +0000

    Create example project using ttcli.

---
+++ src/main/java/com/example/intellijttcli/IntelliJTtcliApplication.java
@@ -0,0 +1,13 @@
+package com.example.intellijttcli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelliJTtcliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelliJTtcliApplication.class, args);
+	}
+
+}
